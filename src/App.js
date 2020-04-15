8c047492a7c977d0b5d235b8ebd951bf43fca7c6
gobelinmalin <mariemeline.paus@gmail.com>
2020-04-15 10:49:36 +0200

    Initialize develop

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      < Footer />
+    </div>
+  );
+}
+
+export default App;
